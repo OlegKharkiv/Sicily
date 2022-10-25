11c8050e35af4dfcda1ff957a059a5e0d46a4ba8
OlegKharkiv <dginm6@gmail.com>
2022-10-25 22:03:01 +0200

    Weather vidget added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 $(document).ready(function() {
     $('.slider').slick({
         arrows: true,
@@ -8,37 +10,36 @@ $(document).ready(function() {
         speed: 500,
         easing: 'ease',
         infinite: true,
-        initialSlide: 0,
+        initialSlide: 1,
         autoplay: false,
-        autoplaySpeed: 1500,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        pauseOnDotsHover: true,
-        draggable: false,
-        swipe: true,
-        touchThreshold: 5,
-        touchMove: true,
-        waitForAnimate: false,
-        centerMode: false,
-        variableWidth: false,
-        fade: false,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 400,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ],
+        // autoplaySpeed: 1500,
+        // pauseOnFocus: true,
+        // pauseOnHover: true,
+        // pauseOnDotsHover: true,
+        // draggable: false,
+        // swipe: true,
+        // touchThreshold: 5,
+        // touchMove: true,
+        // waitForAnimate: false,
+        // centerMode: true,
+        // variableWidth: false,
+        // fade: false,
+        // responsive: [
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //             slidesToShow: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 400,
+        //         settings: {
+        //             slidesToShow: 1
+        //         }
+        //     }
+        // ],
     });
 });
-$('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-            console.log(currentSlide);
-});
-// $('.slider').slick('setPosition');
+
+
+
